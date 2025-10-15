a2bbd95833886a3960e06bb3fe45de723f4fdb77
krishNAutiyal <krishnautiyal4444@gmail.com>
2025-10-15 20:17:45 +0530

    Initial commit

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,26 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define the routes that should be protected
 const protectedRoutes = createRouteMatcher([
-    '/',
-    '/upcoming',
-    '/previous',
-    '/recordings',
-    '/personal-room',
-    '/meetings(.*)' 
-])
+  '/',
+  '/upcoming',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+  '/meetings(.*)',
+]);
 
-export default clerkMiddleware((auth , req) => {
-    if(protectedRoutes(req)) auth().protect();
+// Middleware to protect routes
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) {
+    auth().protect(); // Protect the route
+  }
 });
 
+// Middleware config (which routes it applies to)
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
